120913ffb58569c5545635b5f8ad3826b3b34da9
AndreaSchiariti <schiariti.andrea@gmail.com>
2024-05-15 15:59:15 +0200

    feat: exercise done

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const addProduct = () => {
-  const input = document.querySelector(input);
-
+  const input = document.querySelector("input");
   const addedTask = input.value;
-  
+
+  const ul = document.querySelector("ul")
+  const li = document.createElement("li");
+  li.innerHTML = "<input type='checkbox'></input> " + addedTask
+  ul.appendChild(li)
 };
